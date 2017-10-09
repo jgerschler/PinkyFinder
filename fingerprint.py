94aba8fcd8fe29cc09eceddc1e1922a6bdbd51df
Jared Gerschler <jjgerschler@gmail.com>
2017-10-09 16:58:08 -0500

    testing script

--- fingerprint.py
+++ fingerprint.py
@@ -6,6 +6,8 @@ img = cv2.imread('prints\\101_1.tif', 0)
 
 ret, threshold = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
+threshold = cv2.bitwise_not(threshold)# invert image
+
 plt.subplot(1, 1, 1)
 plt.imshow(threshold, 'gray')
 plt.show()
