9d9a80c5d50dd95c7a8d56c4f392a4d852b98cbb
Jared Gerschler <jjgerschler@gmail.com>
2018-03-04 15:45:34 -0600

    algorithm experimentation

--- fingerprint_SURF.py
+++ fingerprint_SURF.py
@@ -4,13 +4,13 @@ import timeit
 
 img = cv2.imread('thinned.png', 0)
 
-sift = cv2.xfeatures2d.SIFT_create(10)
-keypoints = sift.detect(img, None)
+surf = cv2.xfeatures2d.SURF_create(4000)
+keypoints, descriptors = surf.detectAndCompute(img, None)
 kp_img = cv2.drawKeypoints(img, keypoints, None, (0, 255, 0))
 
 ##img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 
 
-cv2.imshow('SIFT', kp_img)
+cv2.imshow('SURF', kp_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
