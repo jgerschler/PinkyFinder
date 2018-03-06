3f9f7bb13295a0e7087fcca7cd02cd57940587ef
Jared Gerschler <jjgerschler@gmail.com>
2018-03-05 19:02:58 -0600

    in progress

--- fingerprint_ORB.py
+++ fingerprint_ORB.py
@@ -1,10 +1,15 @@
 import cv2
+import time
 
 img = cv2.imread('thinned.png', 0)
 
+t0 = time.clock()
 orb = cv2.ORB_create(10)
 keypoints = orb.detect(img, None)
 keypoints, descriptors = orb.compute(img, keypoints)
+t1 = time.clock()
+
+print(t1-t0)
 
 kp_img = cv2.drawKeypoints(img, keypoints, None, color=(0,255,0), flags=0)
 
