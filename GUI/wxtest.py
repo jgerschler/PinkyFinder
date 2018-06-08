81750a17e1a4ffb78cd9c8ef1ca2e80390f58d09
Jared Gerschler <jjgerschler@gmail.com>
2018-06-08 17:13:06 -0500

    GUI experimentation

--- GUI/wxtest.py
+++ GUI/wxtest.py
@@ -1,17 +1,30 @@
 import wx
 
 
-class Example(wx.Frame):
+class MainFrame(wx.Frame):
     def __init__(self, parent, title, size):
-        super(Example, self).__init__(parent, title=title, size=size)
+        super(MainFrame, self).__init__(parent, title=title, size=size)
+
+        menubar = wx.MenuBar()
+        fileMenu = wx.Menu()
+        fileItem = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit application')
+        menubar.Append(fileMenu, '&File')
+        self.SetMenuBar(menubar)
+
+        self.Bind(wx.EVT_MENU, self.OnQuit, fileItem)
+
         self.Center()
         self.Maximize()
-        
+
+    def OnQuit(self, e):
+        self.Close()
+
+
 def main():
     app = wx.App()
     size = wx.Display().GetGeometry()[2:4]
     width, height = size[0] * 0.9, size[1] * 0.9
-    ex = Example(None, title='Sizing', size=(width, height))
+    ex = MainFrame(None, title='Pinky Finder', size=(width, height))
     ex.Show()
     app.MainLoop()
 
