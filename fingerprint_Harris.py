39a5b725e73bbae3adb15de6c1b6b9702b51184f
Jared Gerschler <jjgerschler@gmail.com>
2018-06-18 13:52:35 -0500

    comparing corner detetion algorithms

--- fingerprint_Harris.py
+++ fingerprint_Harris.py
@@ -1,22 +1,19 @@
 import cv2
 import numpy as np
-import time
 
 img = cv2.imread('thinned.png', 0)
 
-t0 = time.clock()
-corners = cv2.goodFeaturesToTrack(img, 10, 0.01, 3, True)
-t1 = time.clock()
+corners = cv2.goodFeaturesToTrack(img, 20, 0.01, 3, True)
 corners = np.int0(corners)
 
 img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 
 for i in corners:
     x, y = i.ravel()
-    cv2.circle(img, (x, y), 5, (0, 69, 255), 1)
+    cv2.circle(img, (x, y), 5, (255, 0, 0), 1)
 
-print(t1-t0)
+cv2.imwrite('harris.png', img)
 
-cv2.imshow('Shi-Tomasi', img)
+cv2.imshow('Harris', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- fingerprint_Shi_Tomasi.py
+++ fingerprint_Shi_Tomasi.py
@@ -3,14 +3,16 @@ import numpy as np
 
 img = cv2.imread('thinned.png', 0)
 
-corners = cv2.goodFeaturesToTrack(img, 10, 0.01, 3)
+corners = cv2.goodFeaturesToTrack(img, 20, 0.01, 3)
 corners = np.int0(corners)
 
 img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 
 for i in corners:
     x, y = i.ravel()
-    cv2.circle(img, (x, y), 5, (0, 69, 255), 1)
+    cv2.circle(img, (x, y), 5, (255, 0, 0), 1)
+
+cv2.imwrite('shi-tomasi.png', img)
 
 cv2.imshow('Shi-Tomasi', img)
 cv2.waitKey(0)
